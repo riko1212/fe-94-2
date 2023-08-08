3394bc285f610d88eb0bb8ef0419a79e4ab85052
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-08-08 21:42:18 +0300

    +++

--- js/modal.js
+++ js/modal.js
@@ -3,10 +3,16 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', (e) => {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
